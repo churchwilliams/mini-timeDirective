f00daf6c576ae76cd29df639a89a692320762bb8
Eric Williams <eric@churchwilliams.com>
2015-02-26 17:33:34 -0700

    finished. Nice and simple project

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -2,6 +2,4 @@ var app = angular.module('theApp');
 
 app.controller('theCtrl', function($scope){
 	$scope.name = "Eric Church Williams";
-	var currentTime = new Date();
-	$scope.time = currentTime.toString();
 });
--- js/timeDirective.js
+++ js/timeDirective.js
@@ -5,7 +5,8 @@ app.directive('showTime', function(){
 		restrict: 'E',
 		template: '<div> The current time is {{time}} </div>',
 		link: function(scope, element, attrs){
-
+			var currentTime = new Date();
+			scope.time = currentTime.toString();
 		}
 	};
 });
